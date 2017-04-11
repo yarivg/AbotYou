163e2dc7deb847fe74a838b1770d9ca031795426
עומר חן < >
2017-04-11 17:36:22 +0300

    finish stubs and comments to FileHandler class

--- AbotYou/FileHandler.cpp
+++ AbotYou/FileHandler.cpp
@@ -9,3 +9,58 @@ FileHandler::~FileHandler()
 {
 
 }
+
+bool FileHandler::Init(char* path, EFileOCMode ocMode)
+{
+	return false;
+}
+
+bool FileHandler::IsInit() const
+{
+	return false;
+}
+
+bool FileHandler::Open(EFileMode fileMode)
+{
+	return false;
+}
+
+bool FileHandler::Close()
+{
+	return false;
+}
+
+bool FileHandler::Write(uint8_t* buffer, uint64_t length)
+{
+	return false;
+}
+
+uint64_t FileHandler::Read(uint8_t* o_buffer, uint64_t maxLength)
+{
+	return 0;
+}
+
+bool FileHandler::Seek(uint64_t index)
+{
+	return false;
+}
+
+bool FileHandler::IsExist() const
+{
+	return false;
+}
+
+bool FileHandler::Delete() const
+{
+	return false;
+}
+
+bool FileHandler::SafeOpen(EFileMode fileMode)
+{
+	return false;
+}
+
+bool FileHandler::SafeClose()
+{
+	return false;
+}
